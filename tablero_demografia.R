74805df81d17aa5dbfa663d02aaa87dad2f2b3d8
ivan-barrera-mac <10460659+ivan-barrera@users.noreply.github.com>
2026-03-18 21:34:13 -0600

    Adicion de grafica de transicion demografica

--- tablero_demografia.R
+++ tablero_demografia.R
@@ -3,21 +3,24 @@ library(bslib)
 library(tidyverse)
 
 load("./Datos/pob_mit.RData")
+load("./Datos/ind_dem.RData")
 
 ui <- page_fluid(
   selectInput("ent", "Entidad", choices = unique(pob_mit$entidad)),
   selectInput("ano", "Año", choices = unique(pob_mit$ano)),
-  plotOutput("piramide")
+  plotOutput("piramide"),
+  plotOutput("transicion")
   
 )
 
 server <- function(input, output, session) {
 
-  seleccion <- reactive(pob_mit |> filter(entidad == input$ent, ano == input$ano))
+  pob_sel <- reactive(pob_mit |> filter(entidad == input$ent, ano == input$ano))
 
+  tasas_sel <- reactive(ind_dem |> filter(entidad == input$ent))
 
   output$piramide <- renderPlot({
-    seleccion() |>
+    pob_sel() |>
       ggplot(aes(x = poblacion, y = as.factor(edad), fill = sexo)) +
       geom_col(width = 1) + 
       scale_x_continuous(labels = function(x) paste0(abs(x / 1000000), "m")) + 
@@ -30,6 +33,18 @@ server <- function(input, output, session) {
       theme_minimal(base_size = 16) + 
       theme(legend.position = "bottom")
   })
+
+  output$transicion <- renderPlot({
+    tasas_sel() |> 
+      ggplot(aes(x = ano)) +
+      geom_line(aes(y = t_bru_nat, color = "Tasa Bruta de Natalidad")) +
+      geom_line(aes(y = t_bru_mor, color = "Tasa Bruta de Mortalidad")) +
+      scale_color_manual(name = "", values = c("Tasa Bruta de Natalidad" = "blue", "Tasa Bruta de Mortalidad" = "red")) +
+      labs(title = "Transición demográfica", x = "Año", y = "Tasa") +
+      theme_minimal(base_size = 16) +
+      theme(legend.position = "bottom")
+  })
+  
 }
 
 shinyApp(ui, server)
